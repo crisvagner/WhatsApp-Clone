7186bf8f5976162e12a76ae2d9d1ed2819bfc1e7
eokrizz <crisvagnersd@gmail.com>
2023-03-05 18:00:34 -0300

    cria eventos para editar perfil e para add cotanto

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -5,6 +5,7 @@ class WhatsAppController {
 
         this.loadElements();
         this.elementsPrototype();
+        this.initEvents();
     }
 
     loadElements() {
@@ -81,5 +82,50 @@ class WhatsAppController {
             return this.classList.contains(name);
 
         }
+
+        Element.prototype.sleep = function (duration, fn) {
+
+            setTimeout(fn, duration);
+            return this;
+
+        }
+    }
+
+    initEvents() {
+
+        this.el.myPhoto.on('click', event => {
+
+            this.el.panelAddContact.hide();
+            this.el.panelEditProfile.show().sleep(100, () => {
+                this.el.panelEditProfile.addClass('open');
+            });
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', event => {
+
+            this.el.panelEditProfile.removeClass('open').sleep(300, () => {
+                this.el.panelEditProfile.hide();
+            });
+
+        });
+
+        this.el.btnNewContact.on('click', event => {
+
+            this.el.panelEditProfile.hide()
+            this.el.panelAddContact.show().sleep(100, () => {
+                this.el.panelAddContact.addClass('open');
+            });
+
+        });
+
+        this.el.btnClosePanelAddContact.on('click', event => {
+
+            this.el.panelAddContact.removeClass('open').sleep(300, () => {
+                this.el.panelAddContact.hide();
+            });
+
+        });
+
     }
 }
